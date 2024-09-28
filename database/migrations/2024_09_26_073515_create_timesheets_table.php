8a27c59712d2e3b7f5782fa44d611db7fde81f7d
Chanthoeun Kim <chanthoeunkim@gmail.com>
2024-09-28 07:27:57 +0700

    fixed filament security issue

--- database/migrations/2024_09_26_073515_create_timesheets_table.php
+++ database/migrations/2024_09_26_073515_create_timesheets_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
 
         Schema::create('timesheets', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
             $table->date('from_date');
             $table->date('to_date');            
             $table->foreignId('user_id')->constrained()->onDelete('restrict')->cascadeOnUpdate();
